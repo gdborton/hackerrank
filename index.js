c7820784801ee3b53622c4ca4ebebc1c36ecfe60
Gary Borton <gdborton@gmail.com>
2014-08-23 00:33:41 -0400

    Change index.js to have slightly better reporting.

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ var fs = require('fs');
 var challengesFolder = './challenges/';
 
 function solveChallenge (challengeName) {
-
     var solver = require(challengesFolder + challengeName + '/index.js');
     fs.readdir(challengesFolder + challengeName + '/data', function (err, files) {
         var file = '';
         var fileIndex;
         for (fileIndex = 0; fileIndex < files.length; fileIndex++) {
             file = files[fileIndex];
-            fs.readFile('./challenges/' + challengeName + '/data/' + file, { encoding: 'UTF-8' }, function (err, contents) {
-                var startTime = new Date();
-                solver.solve(contents);
-                var endTime = new Date();
-                console.log('Took ' + (endTime - startTime) + ' milliseconds');
-            });
+
+            var contents = fs.readFileSync('./challenges/' + challengeName + '/data/' + file, { encoding: 'UTF-8' });
+            var startTime = new Date();
+            solver.solve(contents);
+            var endTime = new Date();
+            console.log(challengeName, file.split('.')[0] +  ': Took ' + (endTime - startTime) + ' milliseconds.');
         }
     });
 }
@@ -24,7 +23,6 @@ function solveAll () {
         var fileIndex;
         for (fileIndex = 0; fileIndex < files.length; fileIndex++ ) {
             var file = files[fileIndex];
-            console.log('### Solving', file, '###');
             solveChallenge(file)
         }
     });
